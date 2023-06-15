d6c2557404d528827e26aeeeaa31afeeaf7cbe98
saccsokker <toneesworld@outlook.com>
2023-06-14 23:11:06 -0700

    morecode

---
+++ CS 138/Scraggalies/temperaturecalc(F-C).py
@@ -0,0 +1,13 @@
+def main():
+    print ("Welcome to the Fahrenheit to Celsius converter ")
+    #get user input for degrees F
+    x = eval (input (" PLease enter the degrees in Fahrenheit: "))
+    #extra variables for conversion
+    y = 32
+    z = 5/9
+    #Formula
+    sum = (x - y) * z
+    #print out the results
+    print("Degrees in Celsius is:",sum)
+    
+main()
---
+++ CS 138/Scraggalies/temperaturecalc.py
@@ -0,0 +1,10 @@
+#C to F converter
+def main():
+    print ("Welcome to the Celsius to Fahrenheit converter ")
+    x = eval (input (" PLease enter the degrees in Celsius: "))
+    y = 32
+    sum = (x * 1.8) + y
+
+    print("Degrees in Fahrenheit is:",sum)
+    
+main()
