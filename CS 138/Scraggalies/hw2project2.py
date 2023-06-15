368479ce6986b8c0e7fff851c11ac3281a854ea7
saccsokker <toneesworld@outlook.com>
2023-06-14 23:19:12 -0700

    should be good to go

--- CS 138/Scraggalies/hw2project2.py
+++ CS 138/Scraggalies/hw2project2.py
@@ -1,13 +1,13 @@
 def main():
     print ("Welcome to the Fahrenheit to Celsius converter ")
     #get user input for degrees F
-    x = eval (input (" PLease enter the degrees in Fahrenheit that you would like to convert to Celsius: "))
+    x = eval (input ("PLease enter the degrees in Fahrenheit that you would like to convert to Celsius: "))
     #extra variables for conversion
     y = 32
     z = 5/9
     #Formula
     sum = (x - y) * z
     #print out the results
-    print("Degrees in Celsius is:",sum)
+    print(x, "degrees Farenheit ==", sum, "degrees Celsius.")
     
 main()
